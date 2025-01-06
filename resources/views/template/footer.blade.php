ff3a201e79e2c3970be88b1311d56e2a61bbe2d4
Alif-Fikri <aliefmaulanafikri@gmail.com>
2025-01-06 12:28:30 +0700

    template navbar, footer dan page contact us, about, portofolio (blm responsif)

---
+++ resources/views/template/footer.blade.php
@@ -0,0 +1,47 @@
+<head>
+    <link href="{{ asset('css/template/footer.css') }}" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;700&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+</head>
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 col-12 mb-3">
+                <h5 class="footer-title">BEM FASILKOM UMB</h5>
+                <p>Created by PSDM RISTEK</p>
+                <h6 class="footer-title">Get to Know Us</h6>
+                <div class="social-icons">
+                    <a href="#"><i class="bi bi-google"></i></a>
+                    <a href="#"><i class="bi bi-instagram"></i></a>
+                    <a href="#"><i class="bi bi-twitter"></i></a>
+                    <a href="#"><i class="bi bi-facebook"></i></a>
+                </div>          
+            </div>
+            <div class="col-md-2 col-6 mb-3 text-center ms-auto">
+                <h5 class="footer-title">Lorem</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-white">Lorem</a></li>
+                    <li><a href="#" class="text-white">Lorem</a></li>
+                </ul>
+            </div>
+            <div class="col-md-2 col-6 mb-3 text-center ms-auto">
+                <h5 class="footer-title">Lorem</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-white">Lorem 11111</a></li>
+                    <li><a href="#" class="text-white">Lorem 11111</a></li>
+                    <li><a href="#" class="text-white">Lorem 11111</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3 col-12 mb-3 text-end ms-auto">
+                <h5 class="footer-title">Others</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-white">Team</a></li>
+                    <li><a href="#" class="text-white">About Us</a></li>
+                    <li><a href="#" class="text-white">Contact Us</a></li>
+                    <li><a href="#" class="text-white">Help Center</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
